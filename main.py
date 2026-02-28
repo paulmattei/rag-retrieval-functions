2013d61dab2b15a44062cdb871e5694b01f2d7dc
Paul Mattei <paulmattei@pm.me>
2026-02-28 14:36:53 +0000

    Update main.py, utils.py and .gitignore

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import joblib
-import numpy as numpy
+import numpy as np
 import bm25s
 import os
+import transformers
 from sentence_transformers import SentenceTransformer
+
+transformers.logging.set_verbosity_error()
 
 from utils import (
     read_dataframe,
@@ -13,26 +16,6 @@ from utils import (
 )
 
 NEWS_DATA = read_dataframe("news_data_dedup.csv")
-
-
-def query_news(indices):
-    """
-    Retrieves elements from a dataset based on specified indices.
-
-    Parameters:
-    indices (list of int): A list containing the indices of the desired elements in the
-    dataset.
-    dataset(list or sequence): The dataset from which elements are to be retrieved.
-    It should support indexing.
-
-    Returns:
-    list: A list of elements from the dataset corresponding to the indices provided in
-    list_of_indices.
-    """
-
-    output = [NEWS_DATA[index] for index in indices]
-    return output
-
 
 # The corpus used will be the title appended with the description
 corpus = [x["title"] + " " + x["description"] for x in NEWS_DATA]
@@ -45,6 +28,31 @@ tokenised_data = bm25s.tokenize(corpus)
 
 # Index the tokenised chunks within this retriever
 BM25_RETRIEVER.index(tokenised_data)
+
+
+EMBEDDINGS = joblib.load("embeddings.joblib")
+
+model_name = "models/BAAI/bge-base-en-v1.5"
+model = SentenceTransformer(model_name)
+
+
+def query_news(indices):
+    """
+    Retrieves elements from a dataset based on specified indices.
+
+    Parameters:
+    indices (list of int): A list containing the indices of the desired elements in the
+                           dataset.
+    dataset(list or sequence): The dataset from which elements are to be retrieved.
+                               It should support indexing.
+
+    Returns:
+    list: A list of elements from the dataset corresponding to the indices provided in
+    list_of_indices.
+    """
+
+    output = [NEWS_DATA[index] for index in indices]
+    return output
 
 
 def bm25_retrieve(query: str, top_k: int = 5):
@@ -79,5 +87,167 @@ def bm25_retrieve(query: str, top_k: int = 5):
     return top_k_indices
 
 
+def semantic_search_retrieve(query, top_k=5):
+    """
+    Retrieves the top k relevant documents for a given query using semantic search and
+    cosine similarity.
+    This function generates an embedding for the input query and compares it against
+    precomputed document embeddings using cosine similarity. The indices of the top k
+    most similar documents are returned.
+
+    Args:
+        query (str): The search query for which the documents need to be retrieved.
+        top_k (int): The number of top relevant documents to retrieve. Default is 5.
+
+    Returns:
+        List[int]: A list of indices corresponding to the top k relevant documents
+        within the corpus.
+    """
+
+    # Generate the embedding for the query using the pretrained model
+    query_embedding = model.encode(query)
+
+    # Calculate the cosine similarity scores in descending order to get the indices
+    similarity_scores = cosine_similarity(query_embedding, EMBEDDINGS)
+
+    # Sort the similarity scores in descending order and get the indices
+    similarity_indices = np.argsort(-similarity_scores)
+
+    # Select the indices of the top k documents as a numpy array
+    top_k_indices_array = similarity_indices[:top_k]
+
+    # Cast them to int
+    top_k_indices = [int(x) for x in top_k_indices_array]
+
+    return top_k_indices
+
+
+def reciprocal_rank_fusion(list1, list2, top_k=5, K=60):
+    """
+    Fuse rank from multiple IR systems using Reciprocal Rank Fusion.
+
+    Args:
+        list1 (list[int]): A list of indices of the the top k documents that match the
+                           query.
+        list2 (list[int]): Another list of indices of the top k dociments that match the
+                           query.
+        top_k (int): The number of documents to concider from each list for fusion.
+                     Defaults to 5.
+        K (int): A constant used in the RR formula. Defaults to 60.
+
+    Returns:
+        list[int]: A list of indices of the top k documents sorted by their RRF scores.
+    """
+
+    # Create a dictionary to store the RRF score for each document index.
+    rrf_scores = {}
+
+    # Iterate over each document list
+    for lst in [list1, list2]:
+        # Calculate the RRF score for each document index
+        for rank, item in enumerate(
+            lst, start=1
+        ):  # Sets first element as 1 not 0, as is convention.
+            # If the item is not in the dictionary, initialize it's score to 0
+            if item not in rrf_scores:
+                rrf_scores[item] = 0
+            # Update the RRF score for each document index using the formula 1 / (rank + K)
+            rrf_scores[item] += 1 / (rank + K)
+
+    # Sort the document indices based on their RRF scores in decending order
+    sorted_items = sorted(rrf_scores.keys(), key=rrf_scores.get, reverse=True)
+
+    # Slice the list to get the top-k document indices
+    top_k_indices = [int(x) for x in sorted_items[:top_k]]
+
+    return top_k_indices
+
+
+def generate_final_prompt(query, top_k, retrieve_function=None, use_rag=True):
+    """
+    Generates an augmented prompt for a Retrieval Augmented Generation system by
+    retrieving the top_k most relevant documents based on a given query.
+
+    Args:
+        query (str): The search query for which the relevant documents are to be
+                     retrieved.
+        top_k (int): The number of top relevant documents to retrieve.
+        retrieve_function (callable): The function used to retrieve relevant documents.
+                                      If 'reciprocal_rank_fusion', it will combine the
+                                      the results from different retrieval functions.
+        use_rag (bool): A flag to determine whether to incorporate retrieved data into
+                        the prompt (default is True).
+
+        Returns:
+        str: A prompt that includes the top_k relevant documents formatted for use in
+             a RAG system.
+    """
+
+    # Define the prompt as the initial query
+    prompt = query
+
+    # If not using RAG, return the prompt
+    if not use_rag:
+        return prompt
+
+    # Determine which retrieve function to use based on it's name.
+    if retrieve_function.__name__ == "reciprocal_rank_fusion":
+        # Retrieve top documents using two different methods.
+        list1 = semantic_search_retrieve(query, top_k)
+        list2 = bm25_retrieve(query, top_k)
+        # Combine the results using reciprocal rank fusion.
+        top_k_indices = retrieve_function(list1, list2, top_k)
+    else:
+        # Use the provided retrieval function.
+        top_k_indices = retrieve_function(query=query, top_k=top_k)
+
+    # Retrieve documents from the dataset using the indices.
+    relevant_documents = query_news(top_k_indices)
+
+    formatted_documents = []
+
+    for document in relevant_documents:
+        formatted_document = (
+            f"Title: {document['title']}, Description: {document['description']}, "
+            f"Published at: {document['published_at']}\nURL: {document['url']}"
+        )
+
+        # Append the formatted string to the main data string with a newline
+        # for seperation.
+        formatted_documents.append(formatted_document)
+
+    retrieve_data_formatted = "\n".join(formatted_documents)
+
+    prompt = (
+        f"Answer the user query below. There will be provided additional information "
+        f"for you to compose your answer. The relevant infomration provided is from "
+        f"2024 and it should be added as your overall knowledge to answer the query, "
+        f"you should not rely only on this infomration to answer the query, but add it "
+        f"to your overall knowledge.\n"
+        f"Query: {query}\n"
+        f"2024 News: {retrieve_data_formatted}"
+    )
+
+    return prompt
+
+
+def llm_call(query, retrieve_function=None, top_k=5, use_rag=True):
+    # Get the system and user dictionaires
+    prompt = generate_final_prompt(
+        query, top_k=top_k, retrieve_function=retrieve_function, use_rag=use_rag
+    )
+    generated_response = generate_with_single_input(prompt)
+    generated_message = generated_response["content"]
+    return generated_message
+
+
+def main():
+    query = "What do we know about GDP?"
+    #    keyword_list = bm25_retrieve(query)
+    #    semantic_list = semantic_search_retrieve(query)
+    #    reciprocal_rank_fusion(keyword_list, semantic_list)
+    print(llm_call(query, retrieve_function=reciprocal_rank_fusion))
+
+
 if __name__ == "__main__":
-    bm25_retrieve("What about GDP?")
+    main()
--- utils.py
+++ utils.py
@@ -8,12 +8,12 @@ from together import Together
 import numpy as np
 
 
-# Distance formulas. 
+# Distance formulas.
 # In this ungraded lab, distance formulas will be implemented here. However, in future assignments, you will import functions from specialized libraries.
 def cosine_similarity(v1, array_of_vectors):
     """
     Compute the cosine similarity between a vector and an array of vectors.
-    
+
     Parameters:
     v1 (array-like): The first vector.
     array_of_vectors (array-like): An array of vectors or a single vector.
@@ -25,11 +25,11 @@ def cosine_similarity(v1, array_of_vectors):
     v1 = np.array(v1)
     # Initialize a list to store similarities
     similarities = []
-    
+
     # Check if array_of_vectors is a single vector
     if len(np.shape(array_of_vectors)) == 1:
         array_of_vectors = [array_of_vectors]
-    
+
     # Iterate over each vector in the array
     for v2 in array_of_vectors:
         # Convert the current vector to a numpy array
@@ -44,10 +44,11 @@ def cosine_similarity(v1, array_of_vectors):
         similarities.append(similarity)
     return np.array(similarities)
 
+
 def euclidean_distance(v1, array_of_vectors):
     """
     Compute the Euclidean distance between a vector and an array of vectors.
-    
+
     Parameters:
     v1 (array-like): The first vector.
     array_of_vectors (array-like): An array of vectors or a single vector.
@@ -59,18 +60,20 @@ def euclidean_distance(v1, array_of_vectors):
     v1 = np.array(v1)
     # Initialize a list to store distances
     distances = []
-    
+
     # Check if array_of_vectors is a single vector
     if len(np.shape(array_of_vectors)) == 1:
         array_of_vectors = [array_of_vectors]
-    
+
     # Iterate over each vector in the array
     for v2 in array_of_vectors:
         # Convert the current vector to a numpy array
         v2 = np.array(v2)
         # Check if the input arrays have the same shape
         if v1.shape != v2.shape:
-            raise ValueError(f"Shapes don't match: v1 shape: {v1.shape}, v2 shape: {v2.shape}")
+            raise ValueError(
+                f"Shapes don't match: v1 shape: {v1.shape}, v2 shape: {v2.shape}"
+            )
         # Calculate the Euclidean distance and append to the list
         dist = np.sqrt(np.sum((v1 - v2) ** 2))
         distances.append(dist)
@@ -84,71 +87,91 @@ def format_date(date_string):
     formatted_date = date_object.strftime("%Y-%m-%d")
     return formatted_date
 
+
 def pprint(*args, **kwargs):
-    kwargs.setdefault('sort_dicts', False)
+    kwargs.setdefault("sort_dicts", False)
     original_pprint(*args, **kwargs)
-    
-def generate_with_single_input(prompt: str, 
-                               role: str = 'assistant', 
-                               top_p: float = 0, 
-                               temperature: float = 0,
-                               max_tokens: int = 500,
-                               model: str ="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
-                               together_api_key = None,
-                              **kwargs):
-    
+
+
+def generate_with_single_input(
+    prompt: str,
+    role: str = "assistant",
+    top_p: float = 0,
+    temperature: float = 0,
+    max_tokens: int = 500,
+    model: str = "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
+    together_api_key=None,
+    **kwargs,
+):
+
     if top_p is None:
-        top_p = 'none'
+        top_p = "none"
     if temperature is None:
-        temperature = 'none'
+        temperature = "none"
 
     payload = {
-            "model": model,
-            "messages": [{'role': role, 'content': prompt}],
-            "top_p": top_p,
-            "temperature": temperature,
-            "max_tokens": max_tokens,
-            **kwargs
-                  }
-    if (not together_api_key) and ('TOGETHER_API_KEY' not in os.environ):
-        url = os.path.join('https://proxy.dlai.link/coursera_proxy/together', 'v1/chat/completions')   
-        response = requests.post(url, json = payload, verify=False)
+        "model": model,
+        "messages": [{"role": role, "content": prompt}],
+        "top_p": top_p,
+        "temperature": temperature,
+        "max_tokens": max_tokens,
+        **kwargs,
+    }
+    if (not together_api_key) and ("TOGETHER_API_KEY" not in os.environ):
+        url = os.path.join(
+            "https://proxy.dlai.link/coursera_proxy/together", "v1/chat/completions"
+        )
+        response = requests.post(url, json=payload, verify=False)
         if not response.ok:
             raise Exception(f"Error while calling LLM: f{response.text}")
         try:
             json_dict = json.loads(response.text)
         except Exception as e:
-            raise Exception(f"Failed to get correct output from LLM call.\nException: {e}\nResponse: {response.text}")
+            raise Exception(
+                f"Failed to get correct output from LLM call.\nException: {e}\nResponse: {response.text}"
+            )
     else:
         if together_api_key is None:
-            together_api_key = os.environ['TOGETHER_API_KEY']
-        client = Together(api_key =  together_api_key)
+            together_api_key = os.environ["TOGETHER_API_KEY"]
+        client = Together(api_key=together_api_key)
         json_dict = client.chat.completions.create(**payload).model_dump()
-        json_dict['choices'][-1]['message']['role'] = json_dict['choices'][-1]['message']['role'].name.lower()
+        json_dict["choices"][-1]["message"]["role"] = json_dict["choices"][-1][
+            "message"
+        ]["role"]
     try:
-        output_dict = {'role': json_dict['choices'][-1]['message']['role'], 'content': json_dict['choices'][-1]['message']['content']}
+        output_dict = {
+            "role": json_dict["choices"][-1]["message"]["role"],
+            "content": json_dict["choices"][-1]["message"]["content"],
+        }
     except Exception as e:
-        raise Exception(f"Failed to get correct output dict. Please try again. Error: {e}")
+        raise Exception(
+            f"Failed to get correct output dict. Please try again. Error: {e}"
+        )
     return output_dict
 
+
 # Read the CSV without parsing dates
+
 
 def read_dataframe(path):
     df = pd.read_csv(path)
 
     # Apply the custom date formatting function to the relevant columns
-    df['published_at'] = df['published_at'].apply(format_date)
-    df['updated_at'] = df['updated_at'].apply(format_date)
+    df["published_at"] = df["published_at"].apply(format_date)
+    df["updated_at"] = df["updated_at"].apply(format_date)
 
     # Convert the DataFrame to dictionary after formatting
-    df= df.to_dict(orient='records')
+    df = df.to_dict(orient="records")
     return df
 
 
 import ipywidgets as widgets
 from IPython.display import display, Markdown
 
-def display_widget(llm_call_func, semantic_search_retrieve, bm25_retrieve, reciprocal_rank_fusion):
+
+def display_widget(
+    llm_call_func, semantic_search_retrieve, bm25_retrieve, reciprocal_rank_fusion
+):
     def on_button_click(b):
         query = query_input.value
         top_k = slider.value
@@ -163,31 +186,33 @@ def display_widget(llm_call_func, semantic_search_retrieve, bm25_retrieve, recip
             (output1, llm_call_func, query, True, top_k, semantic_search_retrieve),
             (output2, llm_call_func, query, True, top_k, bm25_retrieve),
             (output3, llm_call_func, query, True, top_k, reciprocal_rank_fusion),
-            (output4, llm_call_func, query, False, top_k, None)
+            (output4, llm_call_func, query, False, top_k, None),
         ]
         for output, func, query, use_rag, top_k, retriever in results:
-            response = func(query=query, use_rag=use_rag, top_k=top_k, retrieve_function=retriever)
+            response = func(
+                query=query, use_rag=use_rag, top_k=top_k, retrieve_function=retriever
+            )
             with output:
                 display(Markdown(response))
         # Clear "Generating..." message
         status_output.clear_output()
-        
+
     query_input = widgets.Text(
-        description='',
-        placeholder='Type your query here',
-        layout=widgets.Layout(width='100%')
+        description="",
+        placeholder="Type your query here",
+        layout=widgets.Layout(width="100%"),
     )
-    
+
     slider = widgets.IntSlider(
         value=5,
         min=1,
         max=20,
         step=1,
-        description='Top K:',
-        style={'description_width': 'initial'}
+        description="Top K:",
+        style={"description_width": "initial"},
     )
-    
-    output_style = {'border': '1px solid #ccc', 'width': '100%'}
+
+    output_style = {"border": "1px solid #ccc", "width": "100%"}
     output1 = widgets.Output(layout=output_style)
     output2 = widgets.Output(layout=output_style)
     output3 = widgets.Output(layout=output_style)
@@ -196,7 +221,7 @@ def display_widget(llm_call_func, semantic_search_retrieve, bm25_retrieve, recip
 
     submit_button = widgets.Button(
         description="Get Responses",
-        style={'button_color': '#eee', 'font_color': 'black'}
+        style={"button_color": "#eee", "font_color": "black"},
     )
     submit_button.on_click(on_button_click)
 
@@ -205,7 +230,8 @@ def display_widget(llm_call_func, semantic_search_retrieve, bm25_retrieve, recip
     label3 = widgets.Label(value="Reciprocal Rank Fusion")
     label4 = widgets.Label(value="Without RAG")
 
-    display(widgets.HTML("""
+    display(
+        widgets.HTML("""
     <style>
         .custom-output {
             background-color: #f9f9f9;
@@ -228,31 +254,35 @@ def display_widget(llm_call_func, semantic_search_retrieve, bm25_retrieve, recip
             border: 1px solid #ddd !important;
         }
     </style>
-    """))
-    
+    """)
+    )
+
     display(query_input, slider, submit_button, status_output)
-    
+
     # Create individual vertical containers for each label and output
-    vbox1 = widgets.VBox([label1, output1], layout={'width': '45%'})
-    vbox2 = widgets.VBox([label2, output2], layout={'width': '45%'})
-    vbox3 = widgets.VBox([label3, output3], layout={'width': '45%'})
-    vbox4 = widgets.VBox([label4, output4], layout={'width': '45%'})
-    
+    vbox1 = widgets.VBox([label1, output1], layout={"width": "45%"})
+    vbox2 = widgets.VBox([label2, output2], layout={"width": "45%"})
+    vbox3 = widgets.VBox([label3, output3], layout={"width": "45%"})
+    vbox4 = widgets.VBox([label4, output4], layout={"width": "45%"})
+
     # HBoxes to arrange two VBoxes in each row
-    hbox_outputs1 = widgets.HBox([vbox1, vbox2], layout={'justify_content': 'space-between'})
-    hbox_outputs2 = widgets.HBox([vbox3, vbox4], layout={'justify_content': 'space-between'})
+    hbox_outputs1 = widgets.HBox(
+        [vbox1, vbox2], layout={"justify_content": "space-between"}
+    )
+    hbox_outputs2 = widgets.HBox(
+        [vbox3, vbox4], layout={"justify_content": "space-between"}
+    )
 
     def style_outputs(*outputs):
         for output in outputs:
-            output.layout.margin = '5px'
-            output.layout.height = '300px'
-            output.layout.padding = '10px'
-            output.layout.overflow = 'auto'
+            output.layout.margin = "5px"
+            output.layout.height = "300px"
+            output.layout.padding = "10px"
+            output.layout.overflow = "auto"
             output.add_class("custom-output")
-            
+
     style_outputs(output1, output2, output3, output4)
-    
+
     # Display two rows with two outputs each
     display(hbox_outputs1)
     display(hbox_outputs2)
-
